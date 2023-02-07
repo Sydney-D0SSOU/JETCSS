012fe93788e90dd3155c471706d49c1ba7079d2e
Sydney-D0SSOU <sydneydossou400@0gmail.com>
2023-02-07 10:43:15 +0100

    Gerer le RESPONSIVE

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        primary:'#dc2626'
+      },
+      fontSize:{
+        pro:'10rem'
+      }
+    },
+  },
+  plugins: [],
+}
